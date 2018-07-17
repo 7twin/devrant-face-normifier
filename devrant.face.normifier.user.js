f2bc542d3293e04fdf2c2ef06ec20c450ba87cc6
7twin <32747235+7twin@users.noreply.github.com>
2018-07-17 22:29:28 +0200

    Fixed rant op avatar not getting replaced

--- devrant.face.normifier.user.js
+++ devrant.face.normifier.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         Devrant Face Normifier
 // @namespace    http://devrant.com/
-// @version      0.1
+// @version      0.2
 // @description  remove all avatar expressions
 // @author       7twin
 // @match        *devrant.com/*
 // @match        *devrant.io/*
+// @run-at       document-body
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+
     $(document).ajaxStop(function () {
         $("img").each(function(){
-            $(this).attr("src",$(this).attr("src").replace(/22-\d/g,"22-1"));
+            $(this).attr("src",$(this).attr("src").replace(/22-\d{1,4}/g,"22-1"));
         });
     });
 })();
